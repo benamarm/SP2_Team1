dfe83325b510efe7d164de223924b8d083ce26d3
Adolphe <A-jhay@hotmail.com>
2017-11-26 13:54:35 +0100

    Methoden LoggedIn() & LoggedOut() toegevoegd aan LogDAO

--- HR/src/database/LogDAO.java
+++ HR/src/database/LogDAO.java
@@ -1,5 +1,51 @@
 package database;
 
-public class LogDAO {
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 
+import gui.Main;
+import logic.User;
+
+public class LogDAO {	
+	
+	public static boolean loggedIn(User u) {
+
+		boolean EventHasBeenLogged = false;
+		Session session = Main.factory.getCurrentSession();
+		session.beginTransaction();
+
+		Query q = session.createNativeQuery(
+				"INSERT INTO logs(log_id, loginemail, tijdstip, type, beschrijving) VALUES(:id, :email, :when, :type, :besch)");
+		q.setParameter("id", "NULL")
+		 .setParameter("email", u.getLoginemail())
+		 .setParameter("when", "CURRENT_TIMESTAMP")
+		 .setParameter("type", "AUTH")
+		 .setParameter("besch", "Logged in: "+u.getNaam()+" "+u.getAchternaam());
+		EventHasBeenLogged = true;
+		
+		session.getTransaction().commit();
+
+		return EventHasBeenLogged;
+	}
+	
+	
+	public static boolean loggedOut(User u) {
+
+		boolean EventHasBeenLogged = false;
+		Session session = Main.factory.getCurrentSession();
+		session.beginTransaction();
+
+		Query q = session.createNativeQuery(
+				"INSERT INTO logs(log_id, loginemail, tijdstip, type, beschrijving) VALUES(:id, :email, :when, :type, :besch)");
+		q.setParameter("id", "NULL")
+		 .setParameter("email", u.getLoginemail())
+		 .setParameter("when", "CURRENT_TIMESTAMP")
+		 .setParameter("type", "AUTH")
+		 .setParameter("besch", "Logged OUT: "+u.getNaam()+" "+u.getAchternaam());
+		EventHasBeenLogged = true;
+
+		session.getTransaction().commit();
+
+		return EventHasBeenLogged;
+	}
 }
