e540c765ebce6ed33c59c0461a6f1965d8f3b4e5
Adolphe <A-jhay@hotmail.com>
2017-11-28 19:49:45 +0100

    Google Books API library toegevoegd

--- HR/src/database/VaardigheidDAO.java
+++ HR/src/database/VaardigheidDAO.java
@@ -13,19 +13,23 @@ import logic.Vaardigheid;
 public class VaardigheidDAO {
 
 	@SuppressWarnings("unchecked")
-public static List<Vaardigheid> getUnchecked(Personeel p){
+	public static List<Vaardigheid>  getUnchecked(Personeel p){
 		List<Vaardigheid> teVerwervenVaardigheden = null;
+		
 		Session session = Main.factory.getCurrentSession();
 		session.beginTransaction();
 		
 		 try {
 			   Query q = session.createQuery("FROM Vaardigheid where checked = 0");
 			    teVerwervenVaardigheden = (List<Vaardigheid>) q.list();
+			  
 		   } catch(Exception e) {
 			   e.printStackTrace();
 			   return null;
 		   }
+		
 		return teVerwervenVaardigheden;
 	}
+	
 
 }
