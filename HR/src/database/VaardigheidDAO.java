caf857e32a0c364ba2b758da9dbe502a0d2e7b35
Mohammed <32927663+benamarm@users.noreply.github.com>
2017-11-29 23:43:54 +0100

    Update VaardigheidDAO.java
    commit toegevoegd + Personeel parameter weggehaald

--- HR/src/database/VaardigheidDAO.java
+++ HR/src/database/VaardigheidDAO.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.query.Query;
-
-//import antlr.debug.Event;
 import gui.Main;
 import logic.Personeel;
 import logic.Vaardigheid;
@@ -13,7 +11,7 @@ import logic.Vaardigheid;
 public class VaardigheidDAO {
 
 	@SuppressWarnings("unchecked")
-	public static List<Vaardigheid>  getUnchecked(Personeel p){
+	public static List<Vaardigheid>  getUnchecked() {
 		List<Vaardigheid> teVerwervenVaardigheden = null;
 		
 		Session session = Main.factory.getCurrentSession();
@@ -22,6 +20,8 @@ public class VaardigheidDAO {
 		 try {
 			   Query q = session.createQuery("FROM Vaardigheid where checked = 0");
 			    teVerwervenVaardigheden = (List<Vaardigheid>) q.list();
+		session.getTransaction().commit();
+
 			  
 		   } catch(Exception e) {
 			   e.printStackTrace();
