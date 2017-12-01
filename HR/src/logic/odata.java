e3913acfc33ef422aabb3112ea5fdb5d3f7be27e
DESKTOP-7NBQEQC\jorne geerts <jorne.geerts@gmail.com>
2017-12-01 08:38:33 +0100

    getEverything method uitgewerkt

--- HR/src/logic/odata.java
+++ HR/src/logic/odata.java
@@ -18,41 +18,31 @@ public class odata {
 
 	System.out.println(s);
 	}
+//employeid,lastname,firstname,title,
+	
+	public static String getFirstName(Client client, int EmployeeID) {
+		client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://services.odata.org/V2/Northwind/Northwind.svc/Employees('"+EmployeeID+"')/FirstName?$format=application/json;odata.metadata=none");
+		String s=target.request(MediaType.APPLICATION_JSON).get(String.class);
 
-	public static String getEmail(Client client, String id) {
-	client = ClientBuilder.newClient();
-	WebTarget target = client.target("http://services.odata.org/V2/Northwind/Northwind.svc/Employees('"+id+"')/Emails?$format=application/json;odata.metadata=none");
-	String s=target.request(MediaType.APPLICATION_JSON).get(String.class);
-
-	return s;
-	}
-
-	public static String getFirstName(Client client, String id) {
-	client = ClientBuilder.newClient();
-	WebTarget target = client.target("http://services.odata.org/V2/Northwind/Northwind.svc/Employees('"+id+"')/FirstName?$format=application/json;odata.metadata=none");
-	String s=target.request(MediaType.APPLICATION_JSON).get(String.class);
-
-	return s;
-	}
-
+		return( s);
+		}
+	
 	public static String getLastName(Client client, String id) {
-	client = ClientBuilder.newClient();
-	WebTarget target = client.target("http://services.odata.org/V2/Northwind/Northwind.svc/Employees('"+id+"')/LastName?$format=application/json;odata.metadata=none");
-	String s= target.request(MediaType.APPLICATION_JSON).get(String.class);
-	return s;
-	}
+		client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://services.odata.org/V2/Northwind/Northwind.svc/Employees('"+id+"')/LastName?$format=application/json;odata.metadata=none");
+		String s= target.request(MediaType.APPLICATION_JSON).get(String.class);
+		return s;
+		}
+	
 
-	public static String getAddress(Client client, String id) {
-	client = ClientBuilder.newClient();
-	WebTarget target = client.target("http://services.odata.org/V2/Northwind/Northwind.svc/Employees('"+id+"')/AddressInfo?$format=application/json;odata.metadata=none");
-	String s= target.request(MediaType.APPLICATION_JSON).get(String.class);
-	return s;
+	
 
-	}
+	
 
 		private static void UsernameArray()
 		{
-
+			int EmployeeID = 7;
 		   String[] names  = new String[7];
 
 		    names[0] = "scottketchum";
@@ -61,54 +51,58 @@ public class odata {
 		    names[3] = "javieralfred";
 		    names[4] = "willieashmore";
 		    names[5] = "vincentcalabrese";
-		    names[6]="clydeguess";
+		    names[6]=	"clydeguess";
 
 		    for (String element:names) {
 		   
 		    Client client = ClientBuilder.newClient();
 		    String met=getLastName(client,element);
-		    String sub=met.substring(10);
-		    sub=sub.substring(0, sub.length()-2);
+	    String sub=met.substring(10);
+	    sub=sub.substring(0, sub.length()-2);
 		System.out.println(sub);
 
-		String metho=getFirstName(client,element);
-		    String subs=metho.substring(10);
-		    subs=subs.substring(0, subs.length()-2);
+		String metho=getFirstName(client,EmployeeID);
+	    String subs=metho.substring(10);
+	    subs=subs.substring(0, subs.length()-2);
 		System.out.println(subs);
 
-		String methode=getEmail(client,element);
+		/*String methode=getEmail(client,element);
 		    String submethode=methode.substring(10);
 		    submethode=submethode.substring(0, submethode.length()-2);
 		System.out.println(submethode);
 
 		String m=getAddress(client,element);
 		    String s=m.substring(10);
-		    s=s.substring(0, s.length()-2);
-		    }
+		    s=s.substring(0, s.length()-2);*/
+	    }
 		    }
 		    
 			public  static void main(String[]args) {
-		    	Client client = ClientBuilder.newClient();
+				Client client = ClientBuilder.newClient();
+				 getEverything(client);
+		    	
+		    	
+		    	
 
-		    	String met=getLastName(client,"scottketchum");
+		    	/*String met=getLastName(client,"scottketchum");
 		    	String sub=met.substring(10);
 		    	sub=sub.substring(0, sub.length()-2);
-		    	System.out.println(sub);
+	    	System.out.println(sub);
 
-		    	String metho=getFirstName(client,"scottketchum");
+	    	String metho=getFirstName(client,"scottketchum");
 		    	String subs=metho.substring(10);
-		    	subs=subs.substring(0, subs.length()-2);
+	    	subs=subs.substring(0, subs.length()-2);
 		    	System.out.println(subs);
-
-		    	String methode=getEmail(client,"scottketchum");
-		    	String submethode=methode.substring(10);
-		    	submethode=submethode.substring(0, submethode.length()-2);
-		    	System.out.println(submethode);
-
-		    	String m=getAddress(client,"scottketchum");
-		    	String s=m.substring(10);
-		    	s=s.substring(0, s.length()-2);
-		    	System.out.println(s);
+//
+*///		    	String methode=getEmail(client,"scottketchum");
+//		    	String submethode=methode.substring(10);
+//		    	submethode=submethode.substring(0, submethode.length()-2);
+//		    	System.out.println(submethode);
+//
+//		    	String m=getAddress(client,"scottketchum");
+//		    	String s=m.substring(10);
+//		    	s=s.substring(0, s.length()-2);
+//		    	System.out.println(s);
 
 	    
 	   
