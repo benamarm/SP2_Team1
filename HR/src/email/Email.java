712b43ae1aaff3ffbc4583c0e1255dcde30b88b6
“WoutAmpe” <wout.ampe@hotmail.com>
2017-12-09 17:13:53 +0100

    Methode om mail te sturen naar users als hun aanvraag wordt goed- of afgekeurd

--- HR/src/email/Email.java
+++ HR/src/email/Email.java
@@ -67,4 +67,41 @@ public class Email {
 			e.printStackTrace();
 		}
 	}
+	
+	public static void aanvraag(List<String> to, boolean goedgekeurd) {
+		Iterator<String> iterator = to.iterator();
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+
+		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("noreply.hr.sp2@gmail.com", "+SP2Team1+");
+			}
+		});
+
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("noreply.hr.sp2@gmail.com"));
+			while(iterator.hasNext()) {
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(iterator.next()));
+			}
+			if(goedgekeurd==true) {
+				message.setSubject("Goedkeuring opleiding");
+				message.setText("Beste \n\n\nU krijgt deze mail omdat uw aanvraag is goedgekeurd.\n\n\nGelieve niet te antwoorden op deze mail.");
+			}else if(goedgekeurd==false){
+				message.setSubject("Afkeuring opleiding");
+				message.setText("Beste \n\n\nU krijgt deze mail omdat uw aanvraag NIET is goedgekeurd.\n\n\nGelieve niet te antwoorden op deze mail.");
+			}
+			// send the message
+			Transport.send(message);
+			System.out.println("message sent successfully...");
+
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+	}
 }
