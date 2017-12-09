de49225dbd0a815d95ef582b67d12859a8b4d88c
“WoutAmpe” <wout.ampe@hotmail.com>
2017-12-09 17:10:59 +0100

    Methode om mail te sturen naar user als er een nieuwe user wordt aangemaakt

--- HR/src/email/Email.java
+++ HR/src/email/Email.java
@@ -3,6 +3,8 @@ package email;
 import java.util.Properties;
 import javax.mail.*;
 import javax.mail.internet.*;
+import java.util.Iterator;
+import java.util.List;
 
 public class Email {
 	public static boolean sendPassword(String to, String password) {
@@ -34,5 +36,35 @@ public class Email {
 			return false;
 		}
 		return true;
+	}
+	
+	public static void createdUser(String to, String password) {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+
+		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("noreply.hr.sp2@gmail.com", "+SP2Team1+");
+			}
+		});
+
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("noreply.hr.sp2@gmail.com"));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+			message.setSubject("Account aangemaakt");
+			message.setText("Beste\n\n\nU krijgt deze mail omdat er een account werd aangemaakt met jouw gegevens. \nUw inlog-gegevens zijn:\nUsername: " + to + "\nWachtwoord: " + password + "\n\n\nGelieve niet te antwoorden op deze mail.");
+
+			// send the message
+			Transport.send(message);
+			System.out.println("message sent successfully...");
+
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
 	}
 }
