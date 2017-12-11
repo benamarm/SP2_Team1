6338c054189f8cd71c887ef3e1390a2739786691
“WoutAmpe” <wout.ampe@hotmail.com>
2017-12-11 10:29:29 +0100

    Email methode voor event afkeuring/goedkeuring aangepast

--- HR/src/email/Email.java
+++ HR/src/email/Email.java
@@ -3,11 +3,12 @@ package email;
 import java.util.Properties;
 import javax.mail.*;
 import javax.mail.internet.*;
-import java.util.Iterator;
-import java.util.List;
+import logic.Event;
 
-public class Email {
+public class Email{
 	public static boolean sendPassword(String to, String password) {
+		
+		//properties for connection
 		Properties props = new Properties();
 		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.socketFactory.port", "465");
@@ -15,6 +16,7 @@ public class Email {
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.port", "465");
 
+		//make connection
 		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
 			protected PasswordAuthentication getPasswordAuthentication() {
 				return new PasswordAuthentication("noreply.hr.sp2@gmail.com", "+SP2Team1+");
@@ -22,11 +24,13 @@ public class Email {
 		});
 
 		try {
+			// create message
 			Message message = new MimeMessage(session);
 			message.setFrom(new InternetAddress("noreply.hr.sp2@gmail.com"));
 			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
 			message.setSubject("Nieuw wachtwoord");
-			message.setText("Beste\n\nU krijgt deze mail omdat uw wachtwoord is gewijzigd.\nUw nieuwe wachtwoord is: " + password + "\n \nGelieve niet te antwoorden op deze mail.");
+			message.setText("Beste\n\nU krijgt deze mail omdat uw wachtwoord is gewijzigd.\n"
+					+ "Uw nieuwe wachtwoord is: " + password + "\n \nGelieve niet te antwoorden op deze mail.");
 
 			// send the message
 			Transport.send(message);
@@ -39,13 +43,16 @@ public class Email {
 	}
 	
 	public static void createdUser(String to, String password) {
+		
+		//properties connection
 		Properties props = new Properties();
 		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.socketFactory.port", "465");
 		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.port", "465");
-
+		
+		//make connection
 		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
 			protected PasswordAuthentication getPasswordAuthentication() {
 				return new PasswordAuthentication("noreply.hr.sp2@gmail.com", "+SP2Team1+");
@@ -53,23 +60,25 @@ public class Email {
 		});
 
 		try {
+			// create message
 			Message message = new MimeMessage(session);
 			message.setFrom(new InternetAddress("noreply.hr.sp2@gmail.com"));
 			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
 			message.setSubject("Account aangemaakt");
-			message.setText("Beste\n\n\nU krijgt deze mail omdat er een account werd aangemaakt met jouw gegevens. \nUw inlog-gegevens zijn:\nUsername: " + to + "\nWachtwoord: " + password + "\n\n\nGelieve niet te antwoorden op deze mail.");
+			message.setText("Beste\n\n\nU krijgt deze mail omdat er een account werd aangemaakt met jouw gegevens. \nUw inlog-gegevens zijn:\n"
+					+ "Username: " + to + "\nWachtwoord: " + password + "\n\n\nGelieve niet te antwoorden op deze mail.");
 
 			// send the message
 			Transport.send(message);
-			System.out.println("message sent successfully...");
 
 		} catch (MessagingException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public static void aanvraag(List<String> to, boolean goedgekeurd) {
-		Iterator<String> iterator = to.iterator();
+	public static void aanvraag(String to, boolean goedgekeurd, Event event) {
+		
+		//connection properties
 		Properties props = new Properties();
 		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.socketFactory.port", "465");
@@ -77,6 +86,7 @@ public class Email {
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.port", "465");
 
+		//make connection
 		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
 			protected PasswordAuthentication getPasswordAuthentication() {
 				return new PasswordAuthentication("noreply.hr.sp2@gmail.com", "+SP2Team1+");
@@ -84,21 +94,28 @@ public class Email {
 		});
 
 		try {
+			// create message
 			Message message = new MimeMessage(session);
 			message.setFrom(new InternetAddress("noreply.hr.sp2@gmail.com"));
-			while(iterator.hasNext()) {
-			message.addRecipient(Message.RecipientType.TO, new InternetAddress(iterator.next()));
-			}
-			if(goedgekeurd==true) {
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+			
+			if(goedgekeurd==true) 
+			{
 				message.setSubject("Goedkeuring opleiding");
-				message.setText("Beste \n\n\nU krijgt deze mail omdat uw aanvraag is goedgekeurd.\n\n\nGelieve niet te antwoorden op deze mail.");
-			}else if(goedgekeurd==false){
-				message.setSubject("Afkeuring opleiding");
-				message.setText("Beste \n\n\nU krijgt deze mail omdat uw aanvraag NIET is goedgekeurd.\n\n\nGelieve niet te antwoorden op deze mail.");
+				message.setText("Beste \n\n\nU krijgt deze mail omdat uw aanvraag is goedgekeurd voor volgend event:\nOpleiding:"
+						+ event.getOpleiding().getNaam() + "\nTrainer: " + event.getNaamTrainer() + "\nAdres: " + event.getAdres() + "\nVan " + event.getStartdatum() + " tot " + event.getEinddatum()+ 
+						"\n\n\nGelieve niet te antwoorden op deze mail.");
 			}
+			else if(goedgekeurd==false)
+			{
+				message.setSubject("Afkeuring opleiding");
+				message.setText("Beste \n\n\nU krijgt deze mail omdat uw aanvraag is afgekeurd voor volgend event:\n"
+				+ event.getOpleiding().getNaam() + "\n" + event.getNaamTrainer() + "\n" + event.getAdres() + "\nVan " + event.getStartdatum() + " tot " + event.getEinddatum()+ 
+				"\n\n\nGelieve niet te antwoorden op deze mail.");
+			}
+			
 			// send the message
 			Transport.send(message);
-			System.out.println("message sent successfully...");
 
 		} catch (MessagingException e) {
 			e.printStackTrace();
