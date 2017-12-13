91d6519d5548985e46f88330e2f44182d9cbbfd2
Jordi <jordi.hemelaer@student.ehb.be>
2017-12-13 14:39:16 +0100

    Functies save en getAllOrdered toegevoegd aan klasse AdresDAO

--- HR/src/database/AdresDAO.java
+++ HR/src/database/AdresDAO.java
@@ -1,5 +1,52 @@
 package database;
 
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.util.List;
+import gui.Main;
+import logic.Adres;
+
 public class AdresDAO {
+	
+	public static boolean save(Adres a)
+	{
+		Session session = Main.factory.getCurrentSession();
+		session.beginTransaction();
+		try
+		{
+			session.save(a);
+			session.getTransaction().commit();
+		} catch(Exception e)
+		{
+			e.printStackTrace();
+			
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public static List<Adres> getAllOrdered()
+	{
+		List<Adres> adressen = null;
+		Session session = Main.factory.getCurrentSession();
+		session.beginTransaction();
+		
+		try
+		{
+			Query q = session.createQuery("FROM Adres ORDER BY straat");
+			adressen = (List<Adres>) q.getResultList();
+			session.getTransaction().commit();
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+			
+			
+			return null;
+		}
+		
+		return adressen;
+	}
 
 }
