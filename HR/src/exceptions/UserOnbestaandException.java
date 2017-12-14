bb71d916994d0574495eac6e4a09d80a46a79476
Mohammed <32927663+benamarm@users.noreply.github.com>
2017-12-15 00:03:20 +0100

    Ik zei useless methode...............

--- HR/src/exceptions/UserOnbestaandException.java
+++ HR/src/exceptions/UserOnbestaandException.java
@@ -2,7 +2,5 @@ package exceptions;
 
 @SuppressWarnings("serial")
 public class UserOnbestaandException extends Exception{
-	public void displayErrorMessage() {
-		System.out.println("De user die u zojuist probeerde te raadplegen bestaat niet.");
-	}
+	
 }
